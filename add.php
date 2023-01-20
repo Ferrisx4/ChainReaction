4ef61510d1798902b589d5f3b3197ae122bb20bf
Ferrisx4 <ferrisx4@gmail.com>
2023-01-20 16:37:24 -0500

    Add "suggest" command to suggest words

--- add.php
+++ add.php
@@ -6,9 +6,21 @@ include 'util.php';
 $words = load();
 // Loop through user inputs, appending new words.
 $line ='';
+
+// Get the list of childless words for suggestions.
+$childless = find_childless($words);
+
 while($line != 'end') {
     $line = readline('Word pair (space): ');
     $wordsToAdd = explode(' ', $line);
+    
+    // Suggest a word
+    if($line == 'suggest') {
+        if (count($childless) == 0) { echo "No childless words to suggest!\n"; } else {
+            $rand_word = rand(0,count($childless)-1);
+            echo "Suggestion: " . $childless[$rand_word] . "\n";
+        }
+    }
     if (count($wordsToAdd) > 1) {
         add($wordsToAdd[0],$wordsToAdd[1],$words);
     }
