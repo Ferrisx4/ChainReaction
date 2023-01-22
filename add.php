b6c3bdac96fd4f94fd65ca16c6062af7ae96faf4
Ferrisx4 <ferrisx4@gmail.com>
2023-01-22 17:45:32 -0500

    Better prompt, better random

--- add.php
+++ add.php
@@ -11,13 +11,13 @@ $line ='';
 $childless = find_childless($words);
 
 while($line != 'end') {
-    $line = readline('Word pair (space): ');
+    $line = readline('Enter a new word pair, space separated: ');
     $wordsToAdd = explode(' ', $line);
     
     // Suggest a word
     if($line == 'suggest') {
         if (count($childless) == 0) { echo "No childless words to suggest!\n"; } else {
-            $rand_word = rand(0,count($childless)-1);
+            $rand_word = array_rand($childless);
             echo "Suggestion: " . $childless[$rand_word] . "\n";
         }
     }
