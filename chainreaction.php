85142a9e178ae248bc0e1e6a735348a324dc26a6
Ferrisx4 <ferrisx4@gmail.com>
2023-01-25 00:14:13 -0500

    initial work on actual algorithm

--- chainreaction.php
+++ chainreaction.php
@@ -2,13 +2,18 @@
 
 include 'util.php';
 
-if (count($argv) != 2) {
+if (count($argv) != 4) {
+    echo "Please enter a starting word, an ending word, and the number of words total\n";
     die();
 }
 
+/**
+ * Call this function with 3 arguments: first word, last word, and chain length (inclusive).
+ */
+
 // Get the words.
 $words = load();
-$ws = [];
+$chain = [];
 
 $start = $argv[1];
 $end = $argv[2];
@@ -23,7 +28,18 @@ else {
 }
 
 // Main loop. Check each iteration that we don't use the same word twice.
+// Traverse as deep as the chain length argument - 1
+// 
+function chain_dive(&$words, &$chain, $start, $end, $max_depth, $current_depth) {
+    if ($max_depth == $current_depth && $chain[count($chain)-1] == $end) {
+        echo "breaking\n";
+        return true;
+    }
+    echo $current_depth . "\n";
+    $current_depth += 1;
+    foreach ($)
+    chain_dive($start, $end, $max_depth, $current_depth);
+}
 
-while ()
-
-
+array_push($chain,$start);
+chain_dive($words,$chain,'one','last',15,1);
