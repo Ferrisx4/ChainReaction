ef086161fc804d4e226972b2bd27c6ad4e8bd9d3
Ferrisx4 <ferrisx4@gmail.com>
2023-01-27 16:33:03 -0500

    begin algo

--- chainreaction.php
+++ chainreaction.php
@@ -20,7 +20,7 @@ $end = $argv[2];
 
 // Initialize the stack with the Start word.
 if (array_key_exists($start,$words)) {
-    array_push($ws,$start);
+    array_push($chain,$start);
 }
 else {
     echo "Word not in list\n";
@@ -30,16 +30,36 @@ else {
 // Main loop. Check each iteration that we don't use the same word twice.
 // Traverse as deep as the chain length argument - 1
 // 
-function chain_dive(&$words, &$chain, $start, $end, $max_depth, $current_depth) {
-    if ($max_depth == $current_depth && $chain[count($chain)-1] == $end) {
-        echo "breaking\n";
-        return true;
+function chain_dive(&$words, &$chain, $start, $end, $max_depth) {
+    echo "Testing " . $start . "\n";
+    array_push($chain, $start);
+
+    
+    // Case: Valid chain
+    if ($max_depth == count($chain) && $chain[count($chain)-1] == $end) {
+      echo "breaking\n";
+      return true;
     }
-    echo $current_depth . "\n";
-    $current_depth += 1;
-    foreach ($)
-    chain_dive($start, $end, $max_depth, $current_depth);
+    // Case: Reached max depth (chain length) without finding $end
+    if ($max_depth == count($chain) && $chain[count($chain)-1] != $end) {
+        // The most recently added word does not contribute, pop it off.
+        echo $start . " aint it\n";
+        array_pop($chain);
+        return false;
+    }
+    // Case: Not at max depth, current test doesn't have children (so not valid chain)
+    if (!array_key_exists($start, $words)) {
+        echo $start . " has no children, rejecting\n";
+        array_pop($chain);
+        return false;
+    }
+    // Case: we haven't reached the max depth yet, continue
+    echo "Depth: " . count($chain) . "\n";
+    foreach ($words[$start] as $child) {
+        chain_dive($words, $chain, $child, $end, $max_depth);
+    }
 }
 
-array_push($chain,$start);
-chain_dive($words,$chain,'one','last',15,1);
+chain_dive($words,$chain,$argv[1],$argv[2],$argv[3]);
+
+print_r($chain);
