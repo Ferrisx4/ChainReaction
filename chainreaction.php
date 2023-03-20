652967f63763e204c5714924c0feb0ed2d922978
Ferrisx4 <ferrisx4@gmail.com>
2023-03-20 17:05:24 -0400

    First bad attempt

--- chainreaction.php
+++ chainreaction.php
@@ -17,6 +17,7 @@ $chain = [];
 
 $start = $argv[1];
 $end = $argv[2];
+$chain_length = $argv[3];
 
 // Initialize the stack with the Start word.
 if (array_key_exists($start,$words)) {
@@ -27,39 +28,115 @@ else {
     die();
 }
 
+$chain_found = FALSE;
+$final_chain = [];
 // Main loop. Check each iteration that we don't use the same word twice.
 // Traverse as deep as the chain length argument - 1
 // 
-function chain_dive(&$words, &$chain, $start, $end, $max_depth) {
-    echo "Testing " . $start . "\n";
-    array_push($chain, $start);
+function chain_dive(&$words, &$chain, $next, $end, $chain_length, &$depth) {
+    global $chain_found;
+    global $final_chain;
 
+    echo "Chain at entrance to dive: \n";
+    print_r($chain);
+    echo "at depth: " . $depth . "\n";
+    echo "\n";
+
+    if ($chain_found) {
+        return $chain;
+    }
+    // Case: Valid chain found!
+    elseif ($depth == $chain_length && $chain[count($chain)-1] == $end) {
+        echo "breaking\n";
+        $chain_found = TRUE;
+        $final_chain = $chain;
+        return 'success';
+    }
+    // Case: Chain is not at max depth yet
+    // Action: Recurse into $next's children
+    elseif ($depth < $chain_length) {
+        echo "not at chain_length yet! (depth = " . $depth . ")\n";
+        //sleep(1);
+        if (array_key_exists($next,$words)) {
+            $depth += 1;
+            foreach ($words[$next] as $next_child) {
+                if (!$chain_found) {  
+                echo "testing " . $next . " > " . $next_child . "\n";
+                //print_r($chain);
+                echo "\n";
+                // if $next is already in chain, do nothing
+                if (in_array($next_child, $chain) && $depth != 1) {
+                    echo $next_child . " is already in the chain:\n";
+                    //print_r($chain);
+                    return 'already_in_chain';
+                }
+                // if $next is childless, do nothing
     
-    // Case: Valid chain
-    if ($max_depth == count($chain) && $chain[count($chain)-1] == $end) {
-      echo "breaking\n";
-      return true;
+    
+                array_push($chain, $next_child);
+                    $action = chain_dive($words,$chain,$next_child,$end,$chain_length,$depth);
+                    if ($action == 'reached_chain_length') {
+                        echo "reached_chain_length\n";
+                        $depth -= 1;
+                        array_pop($chain);
+                    }
+                    elseif ($action == 'no_child') {
+                        echo $next_child . " has no child, ending traversal\n";
+                        $depth -= 1;
+                        $popped = array_pop($chain);
+                        echo "popped " . $popped . " off\n";
+                    }
+                    elseif ($action == 'success')
+                    {
+                        echo "Action = success\n";
+                        return 'success';
+                    }
+                    else {
+                        // At this point lets assume this word didn't pan out and pop it, but don't decrement depth.
+                        echo "Unhandled action";
+                        echo "\tCurrent chain: \n";
+                        print_r($chain);
+                        echo "\tNext word: " . $next_child . "\n";
+                        echo "\tChain length: " . $chain_length . "\n";
+                        echo "\tDepth: " . $depth . "\n\n";
+                        array_pop($chain);
+                    }
+                }
+            }
+        }
+        else {
+            echo $next . " was not in the words list! (depth: " . $depth . ")\n";
+            return 'no_child';
+        }
     }
-    // Case: Reached max depth (chain length) without finding $end
-    if ($max_depth == count($chain) && $chain[count($chain)-1] != $end) {
-        // The most recently added word does not contribute, pop it off.
-        echo $start . " aint it\n";
-        array_pop($chain);
-        return false;
+    // Case: Reached designated chain length but the chain is not solved
+    // Action: decrement depth
+    //         pop the end off the array
+    elseif ($depth == $chain_length) {
+        echo "\nChain is at depth, but not solved\n";
+        print_r($chain);
+        echo "Depth: " . $depth . "\n";
+        echo "\n";
+        return 'reached_chain_length';
     }
-    // Case: Not at max depth, current test doesn't have children (so not valid chain)
-    if (!array_key_exists($start, $words)) {
-        echo $start . " has no children, rejecting\n";
-        array_pop($chain);
-        return false;
-    }
-    // Case: we haven't reached the max depth yet, continue
-    echo "Depth: " . count($chain) . "\n";
-    foreach ($words[$start] as $child) {
-        chain_dive($words, $chain, $child, $end, $max_depth);
+    // Case: Reached end of depth-first traversal while less depth < chain_length
+    /*elseif ($depth < $chain_length && !in_array($next, $words)) {
+        //echo "Found word with no children, depth less than desired length\n";
+        return 'no_child';
+    }*/
+    // Catch-all for other conditions
+    else {
+        echo "\nUnhandled condition, printing...\n";
+        echo "\tCurrent chain: \n";
+        print_r($chain);
+        echo "\tNext word: " . $next . "\n";
+        echo "\tChain length: " . $chain_length . "\n";
+        echo "\tDepth: " . $depth . "\n";
+        echo "END OF UNHANDLED CONDITION\n\n";
     }
 }
 
-chain_dive($words,$chain,$argv[1],$argv[2],$argv[3]);
-
-print_r($chain);
+$depth = 1;
+$chain_found = chain_dive($words,$chain,$start,$end,$chain_length,$depth);
+echo "\nFinal: \n";
+print_r($final_chain);
